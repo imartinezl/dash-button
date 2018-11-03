1c475435074be4ef35e9bb0356ab087cb681d8ca
imartinezl <inigomlap@gmail.com>
2018-11-03 12:15:58 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//Find MAC Address
+//cd node_modules/node-dash-button
+//sudo node bin/findbutton
+
+require('dotenv').config();
+
+var dash_button = require('node-dash-button');
+var dash = dash_button(process.env.ADDRESS, null, null, 'all');
+
+var {IncomingWebhook} = require('@slack/client');
+var url =  process.env.SLACK_WEBHOOK_URL; // TEST
+var webhook = new IncomingWebhook(url);
+var mins = 1 // 12 minutes to brew a pot
+
+dash.on("detected", function (){
+	payload={
+		"text": "Come and join us!! Meeting with food :cake:"
+	}
+
+  sendWebhook = function () {
+    webhook.send(payload, function(err, res) {
+	    if (err) {
+	        console.log('Error:', err);
+	    } else {
+	        console.log('Message sent: ', res);
+      }
+    });
+  }
+  setTimeout(sendWebhook, mins);
+});
